29ad081a64a228b1db473352aecdc597d22cf430
rxg9527 <rxg9527@sina.cn>
2021-12-15 19:30:41 +0800

    oclint_privacy_intel.rb

--- oclint_privacy_intel.rb
+++ oclint_privacy_intel.rb
@@ -1,11 +1,11 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class OclintPrivacy < Formula
-  desc "oclint version 21.05 support , privacy rule add"
+class OclintPrivacyIntel < Formula
+  desc "oclint version 21.10 support , privacy rule add"
   homepage "http://oclint.org"
-  url "https://github.com/rxg9527/oclint_privacy/releases/download/v0.1/oclint_privacy.zip"
-  sha256 "22b66ff79338fa3c87b0a9feff282dbae6f513eab351cb0e8ae96e7d46d9f89a"
+  url "https://github.com/rxg9527/oclint_privacy_intel/releases/download/v0.1/oclint_privacy_intel.zip"
+  sha256 "304098e150bc8e04c1de0cd2361bba958839c97f27f8f0b6469501a578834bc9"
   license ""
 
   # depends_on "cmake" => :build
@@ -17,7 +17,7 @@ class OclintPrivacy < Formula
     # system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
 
-    clang_version = '12.0.1'
+    clang_version = '13.0.0'
 
     include.install Dir['include/c++'] unless File.directory? "#{include}/c++"
     "#{include}/c++".install Dir['include/c++/v1'] unless File.directory? "#{include}/c++/v1"
@@ -25,7 +25,7 @@ class OclintPrivacy < Formula
     "#{lib}/clang".install Dir['lib/clang/#{clang_version}'] unless File.directory? "#{lib}/clang/#{clang_version}"
     lib.install Dir['lib/oclint']
     bin.install Dir['bin/*']
-  
+
   end
 
   test do
@@ -33,7 +33,7 @@ class OclintPrivacy < Formula
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
     # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test oclint_privacy`. Options passed
+    # software. Run the test with `brew test oclint_privacy_intel`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any
