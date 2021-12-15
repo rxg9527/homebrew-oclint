f258ff61f575fe533d16aeb29feb945ab4dcfff3
ruanxiaogang <ruanxiaogang@58.com>
2021-12-15 19:01:33 +0800

    first commit

---
+++ oclint_privacy.rb
@@ -0,0 +1,43 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class OclintPrivacy < Formula
+  desc "oclint version 21.05 support , privacy rule add"
+  homepage "http://oclint.org"
+  url "https://github.com/rxg9527/oclint_privacy/releases/download/v0.1/oclint_privacy.zip"
+  sha256 "22b66ff79338fa3c87b0a9feff282dbae6f513eab351cb0e8ae96e7d46d9f89a"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+
+    clang_version = '12.0.1'
+
+    include.install Dir['include/c++'] unless File.directory? "#{include}/c++"
+    "#{include}/c++".install Dir['include/c++/v1'] unless File.directory? "#{include}/c++/v1"
+    lib.install Dir['lib/clang'] unless File.directory? "#{lib}/clang"
+    "#{lib}/clang".install Dir['lib/clang/#{clang_version}'] unless File.directory? "#{lib}/clang/#{clang_version}"
+    lib.install Dir['lib/oclint']
+    bin.install Dir['bin/*']
+  
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test oclint_privacy`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
